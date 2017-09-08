1e0cd4d237263fa614d4a0b0936881c8f9a5661b
Veremey <veremey4uk@gmail.com>
2017-09-08 23:53:00 +0300

    video 2

--- static/js/common.js
+++ static/js/common.js
@@ -136,6 +136,7 @@ $(document).ready(function () {
 		}else {
 			// Use HTML5 video
 			document.querySelectorAll('.canvas')[0].style.display = 'none';
+			document.querySelectorAll('.canvas')[1].style.display = 'none';
 		}
 	};
 
